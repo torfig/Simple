3636899fe78c19e935fe308cce6695c547ed59f7
Torfi Þór Gunnarsson <torfig@ccpgames.com>
2016-04-11 09:52:05 +0000

    Added unit tests

---
+++ WebApplication1/Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Success()
+        {
+            Assert.Equal("true", "true");
+        }
+
+        [Fact]
+        public void Fail()
+        {
+            Assert.Equal("true", "false");
+        }
+    }
+}
